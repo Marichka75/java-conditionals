729191fd9d81e977caf956ca64dd1e99fa8ca0e5
Mariya Byehan <maria_byehan@yahoo.com>
2025-05-07 17:30:30 +0200

    main work to be done on branch dev

--- TrafficLights.java
+++ TrafficLights.java
@@ -4,12 +4,20 @@ public class TrafficLights {
         //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
         
         //Si es verde imprime en la terminal: Puedes seguir
+        String trafficLight = "verde";
+            if (trafficLight.equals("verde")) {
+            System.out.println("Puedes seguir");
         //Si es amarillo imprime en la terminal: Ten cuidado
-        //Si es rojo imprime en la terminal: ¡¡¡Para!!!
+            } else if (trafficLight.equals("amarillo")) {
+                System.out.println("Ten cuidado");
+                //Si es rojo imprime en la terminal: ¡¡¡Para!!!
+            } else if (trafficLight.equals("rojo")) {
+                    System.out.println("¡¡¡Para!!!");
         //Si es otro color imprime en la terminal: El semáforo está dañado
-
-        //Cambia el valor de la variable, corre el programa y mira el resultado 
-
+            } else {
+                System.out.println("El semáforo está dañado");
+            }
+        //Cambia el valor de la variable, corre el programa y mira el resultado
     }
     
 }
